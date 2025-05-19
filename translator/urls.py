7ca86f00c45ad3b8c4a9e11b9f51a00e4599eac3
Shamsiddin Kamoliddinov <160200088+bekfurr@users.noreply.github.com>
2025-05-19 14:40:56 +0500

    Update urls.py

--- translator/urls.py
+++ translator/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
+    path('login/', views.custom_login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('data-processor/', views.data_processor, name='data_processor'),
     path('model-trainer/', views.model_trainer, name='model_trainer'),
